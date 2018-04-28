b7a1d4bdff6a514a2cf86503c643578126322bde
Yosafat Bama <yosafatbama.arintoko@gmail.com>
2018-04-28 18:23:23 +0700

    add card

--- pages/pages/program_keahlian.js
+++ pages/pages/program_keahlian.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Layout from "../Container/Content"
 import Index from "../index.js"
+import { Col, Row } from "react-materialize"
 // function mapStateToProps(state) {
 //     return {
 
@@ -10,10 +11,61 @@ import Index from "../index.js"
 
 export default () => (
     <Index>
-       <p>Program Keahlian</p>
+        <Col className="story_top" m={12} s={12}>
+
+            <Row className="child_">
+                <Col m={4} s={12} style={{marginTop:20}}>
+                    <div className="_card" style={linkStyle} >
+
+                    </div>
+                </Col>
+                <Col m={4} s={12} style={{marginTop:20}}>
+                    <div className="_card" style={linkStyle} >
+
+                    </div>
+                </Col>
+                <Col m={4} s={12} style={{marginTop:20}}>
+                    <div className="_card" style={linkStyle} >
+
+                    </div>
+                </Col>
+                <Col m={4} s={12} style={{marginTop:20}}>
+                    <div className="_card" style={linkStyle} >
+
+                    </div>
+                </Col>
+                <Col m={4} s={12} style={{marginTop:20}}>
+                    <div className="_card" style={linkStyle} >
+
+                    </div>
+                </Col>
+                <Col m={4} s={12} style={{marginTop:20}}>
+                    <div className="_card" style={linkStyle} >
+
+                    </div>
+                </Col>
+                <Col m={4} s={12} style={{marginTop:20, marginBottom:20}}>
+                    <div className="_card" style={linkStyle} >
+
+                    </div>
+                </Col>
+                <Col m={4} s={12} style={{marginTop:20}}>
+                    <div className="_card" style={linkStyle} >
+
+                    </div>
+                </Col>
+                <Col m={4} s={12} style={{marginTop:20}}>
+                    <div className="_card" style={linkStyle} >
+
+                    </div>
+                </Col>
+            </Row>
+        </Col>
     </Index>
 )
-
+const linkStyle = {
+    height: 200, borderWidth: 1, borderColor: '#BDBDBD', borderRadius: 10,
+  }
 // class PageHome extends Component {
 //     render() {
 //         return (
